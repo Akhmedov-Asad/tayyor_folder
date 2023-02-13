dc98965da80a40b779d93008bc45434bb24f83fc
Akhmedov-Asad <akhmedovasadbek76@gmail.com>
2023-02-14 01:52:37 +0500

    big maket project

---
+++ js/script.js
@@ -0,0 +1,68 @@
+const contactBlock = document.querySelector('.contact_block');
+const clickContact = document.querySelector('.click');
+contactBlock.addEventListener('click', function (){
+    clickContact.classList.toggle('active')
+})
+
+
+const anons = document.querySelector('#anons');
+const anonsTitles = document.querySelector('#title__anons');
+anons.addEventListener('click', function(){
+    anonsTitles.classList.toggle('actives');
+})
+
+
+
+document.querySelector('#text').addEventListener('focusout', myText);
+document.querySelector('#password').addEventListener('focusout', myPassword);
+
+document.querySelector('#btn').addEventListener('click', function () {
+    myText();
+    myPassword();
+});
+function myText() {
+    let message, text;
+    let errElement = document.getElementById('text');   
+    message = document.getElementById('err');
+    text = document.getElementById('text').value;
+    try{
+        if(text === ''){
+            errElement.classList.remove('valid');
+            errElement.classList.add('err');
+            throw ' bo\`sh'
+        }
+        if(text.length < 4){
+            errElement.classList.remove('valid');
+            errElement.classList.add('err');
+            throw ' 4 tadan ko`\proq kiriting';
+        }
+        if(text.length > 0){
+            errElement.classList.remove('err');
+            errElement.classList.add('valid');
+            message.innerHTML = '';
+        }
+    } catch(e){
+        message.innerHTML = 'Qiymat' + e
+    }
+}
+
+function myPassword() {
+    let message, password;
+    let errElement = document.getElementById('password');
+    message = document.getElementById('err2');
+    password = document.getElementById('password').value;
+    try{
+        if(password == ''){
+            errElement.classList.remove('valid');
+            errElement.classList.add('err');
+            throw ' bo\`sh'
+        }
+        if(password.length > 0){
+            errElement.classList.remove('err');
+            errElement.classList.add('valid');
+            message.innerHTML = '';
+        }
+    } catch(e){
+        message.innerHTML = 'Qiymat' + e
+    }
+}
